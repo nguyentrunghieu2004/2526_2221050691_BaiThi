a8187aebe76c8f71efa6bae27506f09cf0af9874
Nguyen Trung Hieu <hieu02355h@example.com>
2026-06-20 14:15:34 +0700

    Create_project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _2526_2526050691_BaiThi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
